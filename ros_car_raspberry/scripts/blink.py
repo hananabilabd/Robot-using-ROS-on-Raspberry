1f920dbcbb1695e2be2cef29f7e40a8baf0dcde4
hananabilabd <hananabil@live.com>
2019-03-03 01:26:56 +0200

    add project

--- ros_car_raspberry/scripts/blink.py
+++
@@ -1,9 +0,0 @@
-import wiringpi
-import time
-wiringpi.wiringPiSetup()
-wiringpi.pinMode(1,1)
-while True:
-    time.sleep(0.5)
-    wiringpi.digitalWrite(1,1)
-    time.sleep(0.5)
-    wiringpi.digitalWrite(1,0)
